eb77beeaad0cf27671384ba92af2b9f92fb46707
saleo12 <sarahhilfiker71@gmail.com>
2023-03-28 11:36:02 +0200

    hochladen

---
+++ uebung3/figur.py
@@ -0,0 +1,9 @@
+class Figur:
+    def __init__(self):
+        self.name = "Figur"
+
+    def Umfang(self):
+        return 0
+    
+    def __str__(self):
+        return self.name
---
+++ uebung3/unterfiguren.py
@@ -0,0 +1,3 @@
+class Punkt
+
+class Dreieck
